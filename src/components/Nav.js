15b52b44b8189b822de1877021f1e150a71e8a08
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-27 18:09:53 +0800

    Add drop down menu toggle function in side navigation menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Router, Link } from 'react-static'
-// import { hot } from 'react-hot-loader'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,65 +10,118 @@ library.add(fab, faEnvelope, faCaretDown, faTimesCircle)
 
 import '../main.css'
 
-const openSlideMenu = () => {
+const sideMenu = document.getElementById('side-menu')
+console.log(sideMenu)
+
+
+const openSlideMenu = (e) => {
+  e.preventDefault()
   document.getElementById('side-menu').style.width = '300px';
 }
 
-const closeSlideMenu = () => {
+const closeSlideMenu = (e) => {
+  e.preventDefault()
   document.getElementById('side-menu').style.width = '0';
-  removeActiveClass()
-  hideMenu()
 }
 
-const Nav = () => {
-  return (
-    <div>
-      <nav className="navbar">
-        <div id="logo" className="brand"><Link exact to="/">CM Business Solutions</Link></div>
-        <ul className="navbar-nav">
-          <li><Link exact to="/" className="nav-link">Home</Link></li>
-          <li className="dropdown">
-            <a className="nav-link dropbtn">Services{' '}
-              <FontAwesomeIcon icon={faCaretDown} size='lg' />
-            </a>
-            <div className="dropdown-content">
-              <Link to="/software_dev">Software Development</Link>
-              <Link to="/data_services">Data Engineering & Analytics</Link>
-              <Link to="/cio">Virtual CIO</Link>
-            </div>
-          </li>
-          <li><Link to="/about" className="nav-link">About</Link></li>
-          <li><Link to="/contact" className="nav-link contact-btn">Contact</Link></li>
-        </ul>
-        <div onClick={openSlideMenu} id="menu-small">&#9776;</div>
-      </nav>
+class Nav extends Component {
+  constructor() {
+    super();
 
-      <div id="side-menu" className="side-nav">
-        <div className="form-header">
-          <p className="brand">
-            <Link exact to="/">CM Business Solutions</Link>
-          </p>
-          <FontAwesomeIcon onClick={closeSlideMenu} id="close-slide" icon={faTimesCircle} />
+    this.state = {
+      showMenu: false,
+    };
+
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu(event) {
+
+    if (!this.dropdownMenu.contains(event.target)) {
+
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div id="logo" className="brand"><Link exact to="/">CM Business Solutions</Link></div>
+          <ul className="navbar-nav">
+            <li><Link exact to="/" className="nav-link">Home</Link></li>
+            <li className="dropdown">
+              <a className="nav-link dropbtn">Services{' '}
+                <FontAwesomeIcon icon={faCaretDown} size='lg' />
+              </a>
+              <div className="dropdown-content">
+                <Link to="/software_dev">Software Development</Link>
+                <Link to="/data_services">Data Engineering & Analytics</Link>
+                <Link to="/cio">Virtual CIO</Link>
+              </div>
+            </li>
+            <li><Link to="/about" className="nav-link">About</Link></li>
+            <li><Link to="/contact" className="nav-link contact-btn">Contact</Link></li>
+          </ul>
+          <div onClick={openSlideMenu} id="menu-small">&#9776;</div>
+        </nav>
+
+        <div id="side-menu" className="side-nav">
+          <div className="form-header">
+            <p className="brand">
+              <Link exact to="/">CM Business Solutions</Link>
+            </p>
+            <FontAwesomeIcon onClick={closeSlideMenu}  id="close-slide" icon={faTimesCircle} />
+          </div>
+          <hr />
+          <ul>
+            <li className="dropdown-menu-side">
+              <a onClick={this.showMenu} className="dropbtn">Services{' '}
+                <FontAwesomeIcon icon={faCaretDown} size='lg' />
+              </a>
+              {
+                this.state.showMenu ? (
+                  <ul 
+                    className="dropdown-menu show-menu" 
+                    ref={(element) => {
+                      this.dropdownMenu = element;
+                  }}>
+                    <li className="side-nav-link nav-sub-link"><Link to="/software_dev">Software Development</Link></li>
+                    <li className="side-nav-link nav-sub-link"><Link to="/data_services">Data Engineering & Analytics</Link></li>
+                    <li className="side-nav-link nav-sub-link"><Link to="/cio">Virtual CIO</Link></li>
+                  </ul>
+                )
+                : (
+                  <ul className="dropdown-menu hide-menu">
+                    <li className="side-nav-link nav-sub-link"><Link to="/software_dev">Software Development</Link></li>
+                    <li className="side-nav-link nav-sub-link"><Link to="/data_services">Data Engineering & Analytics</Link></li>
+                    <li className="side-nav-link nav-sub-link"><Link to="/cio">Virtual CIO</Link></li>
+                  </ul>
+                )
+              }
+              
+
+            </li>
+            <li className="side-nav-link"><Link to="/about" >About</Link></li>
+            <li className="side-nav-link"><Link to="/contact" className="contact-btn">Contact</Link></li>
+          </ul>
         </div>
-        <hr />
-        <ul>
-          <li className="dropdown-menu-side">
-            <Link to="#" className="dropbtn">Services{' '}
-              <FontAwesomeIcon icon={faCaretDown} size='lg' />
-            </Link>
-            <ul className="dropdown-menu hide-menu">
-              <li className="side-nav-link nav-sub-link"><Link to="/software_dev">Software Development</Link></li>
-              <li className="side-nav-link nav-sub-link"><Link to="/data_services">Data Engineering & Analytics</Link></li>
-              <li className="side-nav-link nav-sub-link"><Link to="/cio">Virtual CIO</Link></li>
-            </ul>
-          </li>
-          <li className="side-nav-link"><Link to="/about" >About</Link></li>
-          <li className="side-nav-link"><Link to="/contact" className="contact-btn">Contact</Link></li>
-        </ul>
-      </div>   
 
-    </div>
-  )
+      </div>
+    )
+  }
 }
 
 export default Nav
