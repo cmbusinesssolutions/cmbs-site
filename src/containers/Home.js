0a593cce694d639bfb400a76f9101ce3f7b0f02b
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-27 18:34:48 +0800

    Add Home page placeholder

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,8 +4,17 @@ import { withSiteData } from 'react-static'
 import logoImg from '../logo.png'
 
 export default withSiteData(() => (
-  <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
+  <div id="home">
+      <header id="intro">
+        <h1 id="headline">CM Business Solutions</h1>
+        <h4 id="subtitle">
+          We ensure you’re doing the RIGHT THINGS, in the RIGHT WAY, at the RIGHT TIME.
+        </h4>
+      </header>
+      <div id="tagline">
+        <p>
+          We work with organizations, large and small, in leveraging technology and data to solve problems and achieve business goals.
+        </p>
+      </div>
   </div>
 ))
