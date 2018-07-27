c73b8b6f30634855e088ea67985920eb9b606fd1
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-27 15:45:45 +0800

    Add navigation menu component

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Router, Link } from 'react-static'
+import { hot } from 'react-hot-loader'
+
+import '../main.css'
+
+const Nav = () => {
+  return (
+    <div>
+      <h1>Navigation Menu</h1>
+    </div>
+  )
+}
+
+export default Nav
