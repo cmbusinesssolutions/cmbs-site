41dbe1f3cbf997e3a9f72642753e08c043acdcf1
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-07-27 16:48:08 +0800

    Add main navigation menu

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Router, Link } from 'react-static'
 import { hot } from 'react-hot-loader'
 //
 import Routes from 'react-static-routes'
+import Nav from './components/Nav'
 
-import './app.css'
+import './main.css'
 
 const App = () => (
   <Router>
     <div>
-      <nav>
+      {/* <nav>
         <Link exact to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
-      </nav>
+      </nav> */}
+      <Nav />
       <div className="content">
         <Routes />
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import { Router, Link } from 'react-static'
-import { hot } from 'react-hot-loader'
+// import { hot } from 'react-hot-loader'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faCaretDown, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faEnvelope, faCaretDown, faTimesCircle)
 
 import '../main.css'
+
+const openSlideMenu = () => {
+  document.getElementById('side-menu').style.width = '300px';
+}
+
+const closeSlideMenu = () => {
+  document.getElementById('side-menu').style.width = '0';
+  removeActiveClass()
+  hideMenu()
+}
 
 const Nav = () => {
   return (
@@ -16,15 +33,15 @@ const Nav = () => {
               <FontAwesomeIcon icon={faCaretDown} size='lg' />
             </a>
             <div className="dropdown-content">
-              <Link to="#">Software Development</Link>
-              <Link to="#">Data Engineering & Analytics</Link>
-              <Link to="#">Virtual CIO</Link>
+              <Link to="/software_dev">Software Development</Link>
+              <Link to="/data_services">Data Engineering & Analytics</Link>
+              <Link to="/cio">Virtual CIO</Link>
             </div>
           </li>
           <li><Link to="/about" className="nav-link">About</Link></li>
-          <li><Link to="#contact" className="nav-link contact-btn">Contact</Link></li>
+          <li><Link to="/contact" className="nav-link contact-btn">Contact</Link></li>
         </ul>
-        <div id="menu-small">&#9776;</div>
+        <div onClick={openSlideMenu} id="menu-small">&#9776;</div>
       </nav>
 
       <div id="side-menu" className="side-nav">
@@ -32,7 +49,7 @@ const Nav = () => {
           <p className="brand">
             <Link exact to="/">CM Business Solutions</Link>
           </p>
-          <i id="close-slide" className="far fa-times-circle"></i>
+          <FontAwesomeIcon onClick={closeSlideMenu} id="close-slide" icon={faTimesCircle} />
         </div>
         <hr />
         <ul>
@@ -41,13 +58,13 @@ const Nav = () => {
               <FontAwesomeIcon icon={faCaretDown} size='lg' />
             </Link>
             <ul className="dropdown-menu hide-menu">
-              <li className="side-nav-link nav-sub-link"><Link to="#">Software Development</Link></li>
-              <li className="side-nav-link nav-sub-link"><Link to="#">Data Engineering & Analytics</Link></li>
-              <li className="side-nav-link nav-sub-link"><Link to="#">Virtual CIO</Link></li>
+              <li className="side-nav-link nav-sub-link"><Link to="/software_dev">Software Development</Link></li>
+              <li className="side-nav-link nav-sub-link"><Link to="/data_services">Data Engineering & Analytics</Link></li>
+              <li className="side-nav-link nav-sub-link"><Link to="/cio">Virtual CIO</Link></li>
             </ul>
           </li>
           <li className="side-nav-link"><Link to="/about" >About</Link></li>
-          <li className="side-nav-link"><Link to="#" className="contact-btn">Contact</Link></li>
+          <li className="side-nav-link"><Link to="/contact" className="contact-btn">Contact</Link></li>
         </ul>
       </div>   
 
