e39dcfb75bd4becefdec15bf587bdea570efb997
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-02 15:37:13 +0800

    Add About Us page content

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,10 +1,65 @@
-
-import React from 'react'
+import React, { Component } from 'react'
+import { createClient } from 'contentful';
 //
 
-export default () => (
-  <div>
-    <h1>This is what we're all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
-  </div>
-)
+ class About extends Component {
+   constructor() {
+     super()
+     this.state = { 
+       title: null,
+       problem: null,
+       valueProp: null,
+       description: null,
+       media: null,
+       cta: null,
+       price: null,
+       demo: null, 
+      }
+   }
+
+  componentWillMount() {
+    const client = createClient({
+      space: 'twlasw34206y',
+      accessToken: 'd4d47521c5c779f043334df8ccbe249ded24ffcbd6755a5034cfb865d02e0058'
+    })
+
+    client.getEntry('11cBA9fgCgUCSKGcOwsgCU')
+      .then(entry => {
+        const content = entry.fields
+        console.log(content)
+        this.setState({ 
+          title: content.itemName,
+          problem: content.problemDescription,
+          valueProp: content.valueProposition,
+          description: content.itemDescription,
+          media: content.itemMedia,
+          cta: content.itemCallToAction,
+          price: content.itemPrice,
+          demo: content.itemDemo,  
+        })
+      }).catch(err => console.log(`Retrieving entry.fieldsful data error: ${err}`))
+
+  }
+
+  render() {
+    return (
+      <div>
+        <title>About US | CM Business Solutions</title>
+        <section id="about" className="section">
+          <div className="container">
+            <h1 className="section-title">{this.state.title}</h1>
+            <div className="section-text">
+              <p>{this.state.problem}</p>
+              <p>{this.state.description}</p>
+              <p>{this.state.valueProp}</p>
+              <p>{this.state.cta}</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+ } 
+ 
+
+export default About
