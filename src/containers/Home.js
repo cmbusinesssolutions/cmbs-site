7b9bc32c159772ae3eb022f6bc591843c1ff10ce
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-06 19:18:03 +0800

    Migrate Home module to Component class

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import { withSiteData } from 'react-static'
+import React, { Component } from 'react'
+import { createClient } from 'contentful'
 //
 
-export default withSiteData(() => (
-  <div>
+class Home extends Component {
+  render () {
+    return (
+      <div>
+        <title>CM Business Solutions</title>
+        <div id="home">
+          <header id="intro">
+            <h1 id="headline">CM Business Solutions</h1>
+            <h4 id="subtitle">
+              We ensure you’re doing the RIGHT THINGS, in the RIGHT WAY, at the RIGHT TIME.
+        </h4>
+          </header>
+          <div id="tagline">
+            <p>
+              We work with organizations, large and small, in leveraging technology and data to solve problems and achieve business goals.
+        </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
+
+// export default withSiteData(() => (
+  {/* <div>
     <title>CM Business Solutions</title>
     <div id="home">
       <header id="intro">
@@ -18,6 +43,5 @@ export default withSiteData(() => (
         </p>
       </div>
     </div>
-  </div>
-
-))
+  </div> */}
+// ))
