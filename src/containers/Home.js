ee1eba28ef0c19f6a50889ae0914e3689cbce599
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-07 20:13:42 +0800

    Add 'Services' section
    Update CSS formatting for Home section
    
    Add 'Learn More' button

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,26 +2,82 @@ import React, { Component } from 'react'
 import { createClient } from 'contentful'
 //
 
+const flattened = (data, name) => {
+  let arr = data.map(entry => entry).filter(item => item.itemName === name)
+  return arr[0]
+}
+
 class Home extends Component {
+  constructor() {
+    super()
+    this.state = { entries: null }
+  }
+
+  componentDidMount() {
+    const client = createClient({
+      space: 'twlasw34206y',
+      accessToken: 'd4d47521c5c779f043334df8ccbe249ded24ffcbd6755a5034cfb865d02e0058'
+    })
+
+    client.getEntries({ 'content_type': 'productAndService'})
+      .then((response) => {
+        let entries = response.items.map(entry => entry.fields)
+        this.setState({entries: entries})
+      })
+      .catch(console.error)
+  }
+
   render () {
-    return (
-      <div>
-        <title>CM Business Solutions</title>
-        <div id="home">
-          <header id="intro">
-            <h1 id="headline">CM Business Solutions</h1>
-            <h4 id="subtitle">
-              We ensure you’re doing the RIGHT THINGS, in the RIGHT WAY, at the RIGHT TIME.
-        </h4>
-          </header>
-          <div id="tagline">
-            <p>
-              We work with organizations, large and small, in leveraging technology and data to solve problems and achieve business goals.
-        </p>
+    let dev
+    let dataSvcs
+    let cio
+    if (this.state.entries) {
+      dev = flattened(this.state.entries, "Software Development")
+      dataSvcs = flattened(this.state.entries, "Data Engineering & Analytics")
+      cio = flattened(this.state.entries, "Virtual CIO")
+
+      return (
+        <div>
+          <title>CM Business Solutions</title>
+          <div id="home">
+            <header id="intro">
+              <h1 id="headline">CM Business Solutions</h1>
+              <h4 id="subtitle">
+                We ensure you’re doing the RIGHT THINGS, in the RIGHT WAY, at the RIGHT TIME.
+              </h4>
+            </header>
+            <div id="tagline">
+              <p>
+                We work with organizations, large and small, in leveraging technology and data to solve problems and achieve business goals.
+              </p>
+            </div>
+            <div className="cta-group">
+              <button className="cta-btn">Learn More</button>
+            </div>
           </div>
+          <section id="services" className="section">
+            <div className="section-text">
+              <h2 className="section-title">Services</h2>
+              <div className="services-wrapper">
+                <div className="services-text">
+                  <h4 className="service-name">{dev.itemName}</h4>
+                  <p>{dev.itemSummary}</p>
+                </div>
+                <div className="services-text">
+                  <h4 className="service-name">{dataSvcs.itemName}</h4>
+                  <p>{dataSvcs.itemSummary}</p>
+                </div>
+                <div className="services-text">
+                  <h4 className="service-name">{cio.itemName}</h4>
+                  <p>{cio.itemSummary}</p>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
-      </div>
-    )
+      )
+    }
+    return <div>Loading...</div>;
   }
 }
 
